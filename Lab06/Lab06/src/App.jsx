1e1eadeae1318cad1ee0b04c1ebd8bbf168e3acf
H82M19\Student <Student>
2025-03-19 17:24:00 +0700

    ve lam tiep

---
+++ Lab06/Lab06/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Body1 from "./components/body/body1";
+import "./App.css"
+
+const App = () => {
+  return (
+    <>
+     <Header />
+      <Body1/>
+     <Footer />
+    </>
+  )
+}
+
+export default App;
